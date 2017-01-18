e3772a161bb54ab67c3a937e227b47ef77664d06
Rafael Dantas Justo <rafael@registro.br>
2017-01-18 08:58:15 -0200

    Fix shadow warning
    See #11

--- internal/cloud/clock_test.go
+++ internal/cloud/clock_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 func TestRealClock_Now(t *testing.T) {
-	var realClock realClock
-	if time.Now().Add(-10 * time.Millisecond).After(realClock.Now()) {
+	var r realClock
+	if time.Now().Add(-10 * time.Millisecond).After(r.Now()) {
 		t.Error("real clock isn't returning the current time")
 	}
 }
