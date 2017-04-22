ec46fb8f79987e44b082cdaccefec5102ce7ff96
Rafael Danras Justo <adm@rafael.net.br>
2017-04-21 22:25:54 -0300

    Improve documentation of the cloud package
    Resolves #43

--- internal/cloud/cloud.go
+++ internal/cloud/cloud.go
@@ -7,16 +7,20 @@ import (
 // Cloud offers all necessary operations to manage backups in the cloud.
 type Cloud interface {
 	// Send uploads the file to the cloud and return the backup archive
-	// information.
+	// information. The upload operation can be cancelled anytime using the
+	// context.
 	Send(ctx context.Context, filename string) (Backup, error)
 
-	// List retrieves all the uploaded backups information in the cloud.
+	// List retrieves all the uploaded backups information in the cloud. The
+	// operation can be cancelled anytime using the context.
 	List(ctx context.Context) ([]Backup, error)
 
-	// Get retrieves a specific backup file and stores it locally in a file. The
-	// filename storing the location of the file is returned.
+	// Get retrieves a specific backup and stores it locally in a file. The
+	// filename where the backup was saved is returned. The operation can be
+	// cancelled anytime using the context.
 	Get(ctx context.Context, id string) (filename string, err error)
 
-	// Remove erase a specific backup from the cloud.
+	// Remove erase a specific backup from the cloud. The operation can be
+	// cancelled anytime using the context.
 	Remove(ctx context.Context, id string) error
 }
