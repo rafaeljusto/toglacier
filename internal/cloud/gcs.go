b23a0948e957b352e8d501e07c8686d9f2a779f0
Rafael Danras Justo <adm@rafael.net.br>
2017-09-08 08:38:02 -0300

    Improve GCS implementation for all actions
    See #89

--- internal/cloud/gcs.go
+++ internal/cloud/gcs.go
@@ -5,7 +5,9 @@ import (
 	"encoding/base64"
 	"io"
 	"os"
+	"path"
 	"strconv"
+	"sync"
 	"time"
 
 	"cloud.google.com/go/storage"
@@ -14,18 +16,40 @@ import (
 	"google.golang.org/api/iterator"
 )
 
+const googleCloudStorageClass = "COLDLINE"
+
+// GoogleCloudStorageConfig stores all necessary parameters to initialize a GCS
+// session.
+type GoogleCloudStorageConfig struct {
+	Project    string
+	BucketName string
+}
+
+// GoogleCloudStorage is the Google solution for storing the backups in the
+// cloud. It uses the Google Cloud Storage Coldline service, as it allows large
+// files for a small price.
 type GoogleCloudStorage struct {
 	Logger log.Logger
 	client *storage.Client
 	bucket *storage.BucketHandle
 }
 
-type GoogleCloudStorageConfig struct {
-	Project    string
-	BucketName string
-}
-
-func NewGoogleCloudStorage(logger log.Logger, ctx context.Context, config GoogleCloudStorageConfig) (*GoogleCloudStorage, error) {
+// NewGoogleCloudStorage initializes the Google Cloud Storage bucket. On error
+// it will return an Error type. To retrieve the desired error you can do:
+//
+//     type causer interface {
+//       Cause() error
+//     }
+//
+//     if causeErr, ok := err.(causer); ok {
+//       switch specificErr := causeErr.Cause().(type) {
+//       case *cloud.Error:
+//         // handle specifically
+//       default:
+//         // unknown error
+//       }
+//     }
+func NewGoogleCloudStorage(ctx context.Context, logger log.Logger, config GoogleCloudStorageConfig) (*GoogleCloudStorage, error) {
 	c, err := storage.NewClient(ctx)
 	if err != nil {
 		return nil, errors.WithStack(newError("", ErrorCodeInitializingSession, err))
@@ -33,7 +57,7 @@ func NewGoogleCloudStorage(logger log.Logger, ctx context.Context, config Google
 
 	bkt := c.Bucket(config.BucketName)
 	err = bkt.Create(ctx, config.Project, &storage.BucketAttrs{
-		StorageClass: "COLDLINE",
+		StorageClass: googleCloudStorageClass,
 	})
 
 	if err != nil {
@@ -47,6 +71,22 @@ func NewGoogleCloudStorage(logger log.Logger, ctx context.Context, config Google
 	}, nil
 }
 
+// Send uploads the file to the cloud and return the backup archive information.
+// If an error occurs it will be an Error type encapsulated in a traceable
+// error. To retrieve the desired error you can do:
+//
+//     type causer interface {
+//       Cause() error
+//     }
+//
+//     if causeErr, ok := err.(causer); ok {
+//       switch specificErr := causeErr.Cause().(type) {
+//       case *cloud.Error:
+//         // handle specifically
+//       default:
+//         // unknown error
+//       }
+//     }
 func (g *GoogleCloudStorage) Send(ctx context.Context, filename string) (Backup, error) {
 	g.Logger.Debugf("cloud: sending file “%s” to google cloud", filename)
 
@@ -55,6 +95,7 @@ func (g *GoogleCloudStorage) Send(ctx context.Context, filename string) (Backup,
 		return Backup{}, errors.WithStack(newError("", ErrorCodeOpeningArchive, err))
 	}
 
+	// TODO: better id for object?
 	obj := g.bucket.Object(strconv.FormatInt(time.Now().UnixNano(), 10))
 	w := obj.NewWriter(ctx)
 
@@ -69,6 +110,22 @@ func (g *GoogleCloudStorage) Send(ctx context.Context, filename string) (Backup,
 	return Backup{}, nil
 }
 
+// List retrieves all the uploaded backups information in the cloud. If an error
+// occurs it will be an Error type encapsulated in a traceable error. To
+// retrieve the desired error you can do:
+//
+//     type causer interface {
+//       Cause() error
+//     }
+//
+//     if causeErr, ok := err.(causer); ok {
+//       switch specificErr := causeErr.Cause().(type) {
+//       case *cloud.Error:
+//         // handle specifically
+//       default:
+//         // unknown error
+//       }
+//     }
 func (g *GoogleCloudStorage) List(ctx context.Context) ([]Backup, error) {
 	g.Logger.Debug("cloud: retrieving list of archives from the google cloud")
 
@@ -82,10 +139,10 @@ func (g *GoogleCloudStorage) List(ctx context.Context) ([]Backup, error) {
 		}
 
 		if err != nil {
-			// TODO
+			// TODO: define error
 		}
 
-		// TODO: Vault name?
+		// TODO: vault name?
 		backups = append(backups, Backup{
 			ID:        objAttrs.Name,
 			CreatedAt: objAttrs.Created,
@@ -98,18 +155,106 @@ func (g *GoogleCloudStorage) List(ctx context.Context) ([]Backup, error) {
 	return backups, nil
 }
 
+// Get retrieves a specific backup file and stores it locally in a file. The
+// filename storing the location of the file is returned.  If an error occurs it
+// will be an Error type encapsulated in a traceable error. To retrieve the
+// desired error you can do:
+//
+//     type causer interface {
+//       Cause() error
+//     }
+//
+//     if causeErr, ok := err.(causer); ok {
+//       switch specificErr := causeErr.Cause().(type) {
+//       case *cloud.Error:
+//         // handle specifically
+//       default:
+//         // unknown error
+//       }
+//     }
 func (g *GoogleCloudStorage) Get(ctx context.Context, ids ...string) (map[string]string, error) {
 	g.Logger.Debugf("cloud: retrieving archives “%v” from the google cloud", ids)
 
-	// TODO
+	var waitGroup sync.WaitGroup
+	jobResults := make(chan jobResult, len(ids))
 
-	return nil, nil
+	for _, id := range ids {
+		waitGroup.Add(1)
+		go g.get(ctx, id, &waitGroup, jobResults)
+	}
+
+	waitGroup.Wait()
+
+	filenames := make(map[string]string)
+	for i := 0; i < len(ids); i++ {
+		result := <-jobResults
+		if result.err != nil {
+			// TODO: if only one file failed we will stop it all?
+			return nil, errors.WithStack(result.err)
+		}
+		filenames[result.id] = result.filename
+	}
+	return filenames, nil
 }
 
+func (g *GoogleCloudStorage) get(ctx context.Context, id string, waitGroup *sync.WaitGroup, result chan<- jobResult) {
+	defer waitGroup.Done()
+
+	backup, err := os.Create(path.Join(os.TempDir(), "backup-"+id+".tar"))
+	if err != nil {
+		result <- jobResult{
+			id:  id,
+			err: errors.WithStack(newError(id, ErrorCodeCreatingArchive, err)),
+		}
+		return
+	}
+	defer backup.Close()
+
+	obj := g.bucket.Object(id)
+	r, err := obj.NewReader(ctx)
+	if err != nil {
+		// TODO: define error
+	}
+	defer r.Close()
+
+	if _, err := io.Copy(backup, r); err != nil {
+		result <- jobResult{
+			id:  id,
+			err: errors.WithStack(newError(id, ErrorCodeCopyingData, err)),
+		}
+		return
+	}
+
+	g.Logger.Infof("cloud: backup “%s” retrieved successfully from the google cloud and saved in temporary file “%s”", id, backup.Name())
+
+	result <- jobResult{
+		id:       id,
+		filename: backup.Name(),
+	}
+}
+
+// Remove erase a specific backup from the cloud. If an error occurs it will be
+// an Error type encapsulated in a traceable error. To retrieve the desired
+// error you can do:
+//
+//     type causer interface {
+//       Cause() error
+//     }
+//
+//     if causeErr, ok := err.(causer); ok {
+//       switch specificErr := causeErr.Cause().(type) {
+//       case *cloud.Error:
+//         // handle specifically
+//       default:
+//         // unknown error
+//       }
+//     }
 func (g *GoogleCloudStorage) Remove(ctx context.Context, id string) error {
 	g.Logger.Debugf("cloud: removing archive %s from the google cloud", id)
 
-	// TODO
+	if err := g.bucket.Object(id).Delete(ctx); err != nil {
+		return errors.WithStack(newError(id, ErrorCodeRemovingArchive, err))
+	}
 
 	g.Logger.Infof("cloud: backup “%s” removed successfully from the google cloud", id)
 	return nil
